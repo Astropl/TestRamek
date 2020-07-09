2d5723020d20ca4763d9fc7b6d80cb5c3c8a3e6e
Astropl <martys.pawel@gmail.com>
2020-07-09 13:11:20 +0200

    Add files via upload

--- kontrahent.h
+++ kontrahent.h
@@ -15,8 +15,37 @@ public:
     explicit Kontrahent(QWidget *parent = nullptr);
     ~Kontrahent();
 
+public slots:
+
+    int zmianaLabela(
+        int godzina, int minuta, int sekunda, int dzien, int miesiac, int rok, int dzienTygodnia);
+    void myfunctiontimer();
+    void wczytajMiasta();
+    void wczytajWojewodztwa();
+        //void DataiCzas();
+
+        private slots :
+            void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_actionDodaj_Miasto_triggered();
+
+    void on_actionDodaj_Wojew_dztwo_triggered();
+
+    //void on_lineEditWczytajNazwa_1_editingFinished();
+
+    void on_lineEditWczytajNazwa_1_textChanged(const QString &arg1);
+
+    void on_actionInfo_triggered();
+
+    void on_comboBoxWczytajMiasta_activated(const QString &arg1);
+
+    void on_comboBoxWczytajMiasta_highlighted(const QString &arg1);
+
 private:
     Ui::Kontrahent *ui;
+    QTimer *timer;
 };
 
 #endif // KONTRAHENT_H
