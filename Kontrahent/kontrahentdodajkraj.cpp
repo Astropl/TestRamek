08c679a7eece4f53bee4f7293b6962bd39874fd5
Astropl <martys.pawel@gmail.com>
2020-09-11 14:47:29 +0200

    Add files via upload

--- Kontrahent/kontrahentdodajkraj.cpp
+++ Kontrahent/kontrahentdodajkraj.cpp
@@ -7,21 +7,7 @@
 using namespace std;
 
 fstream plikOdczytDodajKraj;
-QString file1 = "C:/Defaults/Pliki/1.DB.txt";
-QString file2 = "C:/Defaults/Pliki/2.Kontrahent.txt";
-QString file3 = "C:/Defaults/Pliki/3.Urzadzenie.txt";
-QString file4 = "C:/Defaults/Pliki/4.ZapisKraj.txt";
-QString file5 = "C:/Defaults/Pliki/5.ZapisMiasta.txt";
-QString file6 = "C:/Defaults/Pliki/6.ZapisWojewodztwa.txt";
-QString file7 = "C:/Defaults/Pliki/7.ZapisProducenta.txt";
-QString file8 = "C:/Defaults/Pliki/8.ZapisModel.txt";
-QString file9 = "C:/Defaults/Pliki/9.ZapisNrSeryjny.txt";
-QString file10 = "C:/Defaults/Pliki/10.CheckFlagsInMiasto.txt";
-QString file11 = "C:/Defaults/Pliki/11.CheckFlagsInKraj.txt";
-QString file12 = "C:/Defaults/Pliki/12.CheckFlagsInWojewodztwa.txt";
-QString file13 = "C:/Defaults/Pliki/13.CheckFlagsInKrajKontrahentShow.txt";
-QString file14 = "C:/Defaults/Pliki/14.CheckFlagsInMiastoKontrahentShow.txt";
-QString file15 = "C:/Defaults/Pliki/15.CheckFlagsInWojewodztwoKontrahentShow.txt";
+
 
 
 KontrahentDodajKraj::KontrahentDodajKraj(QWidget *parent) :
@@ -29,7 +15,21 @@ KontrahentDodajKraj::KontrahentDodajKraj(QWidget *parent) :
     ui(new Ui::KontrahentDodajKraj)
 {
     ui->setupUi(this);
-
+    QString file1 = "C:/Defaults/Pliki/1.DB.txt";
+    QString file2 = "C:/Defaults/Pliki/2.Kontrahent.txt";
+    QString file3 = "C:/Defaults/Pliki/3.Urzadzenie.txt";
+    QString file4 = "C:/Defaults/Pliki/4.ZapisKraj.txt";
+    QString file5 = "C:/Defaults/Pliki/5.ZapisMiasta.txt";
+    QString file6 = "C:/Defaults/Pliki/6.ZapisWojewodztwa.txt";
+    QString file7 = "C:/Defaults/Pliki/7.ZapisProducenta.txt";
+    QString file8 = "C:/Defaults/Pliki/8.ZapisModel.txt";
+    QString file9 = "C:/Defaults/Pliki/9.ZapisNrSeryjny.txt";
+    QString file10 = "C:/Defaults/Pliki/10.CheckFlagsInMiasto.txt";
+    QString file11 = "C:/Defaults/Pliki/11.CheckFlagsInKraj.txt";
+    QString file12 = "C:/Defaults/Pliki/12.CheckFlagsInWojewodztwa.txt";
+    QString file13 = "C:/Defaults/Pliki/13.CheckFlagsInKrajKontrahentShow.txt";
+    QString file14 = "C:/Defaults/Pliki/14.CheckFlagsInMiastoKontrahentShow.txt";
+    QString file15 = "C:/Defaults/Pliki/15.CheckFlagsInWojewodztwoKontrahentShow.txt";
     plikOdczytDodajKraj.open(file4.toStdString(),ios::in);
     if (plikOdczytDodajKraj.good() == false)
     {cout<<"Plik nie istnieje!!!";
--- Kontrahent/kontrahentdodajwojewodztwo.cpp
+++ Kontrahent/kontrahentdodajwojewodztwo.cpp
@@ -11,28 +11,28 @@
 using namespace std;
 
 fstream plikOdczytDodajWojewodztwa;
-QString file1 = "C:/Defaults/Pliki/1.DB.txt";
-QString file2 = "C:/Defaults/Pliki/2.Kontrahent.txt";
-QString file3 = "C:/Defaults/Pliki/3.Urzadzenie.txt";
-QString file4 = "C:/Defaults/Pliki/4.ZapisKraj.txt";
-QString file5 = "C:/Defaults/Pliki/5.ZapisMiasta.txt";
-QString file6 = "C:/Defaults/Pliki/6.ZapisWojewodztwa.txt";
-QString file7 = "C:/Defaults/Pliki/7.ZapisProducenta.txt";
-QString file8 = "C:/Defaults/Pliki/8.ZapisModel.txt";
-QString file9 = "C:/Defaults/Pliki/9.ZapisNrSeryjny.txt";
-QString file10 = "C:/Defaults/Pliki/10.CheckFlagsInMiasto.txt";
-QString file11 = "C:/Defaults/Pliki/11.CheckFlagsInKraj.txt";
-QString file12 = "C:/Defaults/Pliki/12.CheckFlagsInWojewodztwa.txt";
-QString file13 = "C:/Defaults/Pliki/13.CheckFlagsInKrajKontrahentShow.txt";
-QString file14 = "C:/Defaults/Pliki/14.CheckFlagsInMiastoKontrahentShow.txt";
-QString file15 = "C:/Defaults/Pliki/15.CheckFlagsInWojewodztwoKontrahentShow.txt";
+
 
 KontrahentDodajWojewodztwo::KontrahentDodajWojewodztwo(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::KontrahentDodajWojewodztwo)
 {
     ui->setupUi(this);
-
+    QString file1 = "C:/Defaults/Pliki/1.DB.txt";
+    QString file2 = "C:/Defaults/Pliki/2.Kontrahent.txt";
+    QString file3 = "C:/Defaults/Pliki/3.Urzadzenie.txt";
+    QString file4 = "C:/Defaults/Pliki/4.ZapisKraj.txt";
+    QString file5 = "C:/Defaults/Pliki/5.ZapisMiasta.txt";
+    QString file6 = "C:/Defaults/Pliki/6.ZapisWojewodztwa.txt";
+    QString file7 = "C:/Defaults/Pliki/7.ZapisProducenta.txt";
+    QString file8 = "C:/Defaults/Pliki/8.ZapisModel.txt";
+    QString file9 = "C:/Defaults/Pliki/9.ZapisNrSeryjny.txt";
+    QString file10 = "C:/Defaults/Pliki/10.CheckFlagsInMiasto.txt";
+    QString file11 = "C:/Defaults/Pliki/11.CheckFlagsInKraj.txt";
+    QString file12 = "C:/Defaults/Pliki/12.CheckFlagsInWojewodztwa.txt";
+    QString file13 = "C:/Defaults/Pliki/13.CheckFlagsInKrajKontrahentShow.txt";
+    QString file14 = "C:/Defaults/Pliki/14.CheckFlagsInMiastoKontrahentShow.txt";
+    QString file15 = "C:/Defaults/Pliki/15.CheckFlagsInWojewodztwoKontrahentShow.txt";
     cout<<"Wczytac itemy z pliku"<<endl;
 
     //---------------------------------------------------------
